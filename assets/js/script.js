3d6ffc190d6c400611f6ec10e60074f073d35fe5
Kazim Ozdil <skozdil@gmail.com>
2024-01-28 04:03:46 +0000

    added future fetch in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,107 @@
 // Create cities array
 var cities = [];
-var apiKey = "9a6989cdb2e9dde02696b2a7a395afa3";
-
-
-// Convert city name to coordinats
-// function cityName2coordinats() {
-
-//   var city = $(this).attr("id", "search-input");
-//   var queryCityURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + " &limit=5&appid=" + apiKey;
-
-//   // Creates a Fetch call for the specific movie button being clicked
-//   fetch(queryCityURL)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       return;
-//     });
-// }
+var apiKey = `9a6989cdb2e9dde02696b2a7a395afa3`;
 
 // Fetch weather info 
-$("#search-button").on("click", function() {
-    preventDefault();
-    var city = $("#search-input").val().trim();
-    console.log(city)
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+// var city = $(`#search-input`).val();
+var city = `London`;
+console.log(city);
 
-    
-
-
-    // Creates a Fetch call for the specific movie button being clicked
-    fetch(queryURL)
+function fetchWeatherInfo() {
+    // Convert city name to coordtinats using geo api
+    var queryUrlGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
+    fetch(queryUrlGeo)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
-        return;
-        // Creating a div to hold the movie
-        var movieDiv = $("<div class='movie'>");
-  
-        // Storing the rating data
-        var rating = data.Rated;
-  
-        // Creating an element to have the rating displayed
-        var pOne = $("<p>").text("Rating: " + rating);
-  
-        // Displaying the rating
-        movieDiv.append(pOne);
-  
-        // Storing the release year
-        var released = data.Released;
-  
-        // Creating an element to hold the release year
-        var pTwo = $("<p>").text("Released: " + released);
-  
-        // Displaying the release year
-        movieDiv.append(pTwo);
-  
-        // Storing the plot
-        var plot = data.Plot;
-  
-        // Creating an element to hold the plot
-        var pThree = $("<p>").text("Plot: " + plot);
-  
-        // Appending the plot
-        movieDiv.append(pThree);
-  
-        // Retrieving the URL for the image
-        var imgURL = data.Poster;
-  
-        // Creating an element to hold the image
-        var image = $("<img>").attr("src", imgURL);
-  
-        // Appending the image
-        movieDiv.append(image);
-  
-        // Putting the entire movie above the previous movies
-        $("#movies-view").prepend(movieDiv);
-      });
-  });
+        // Coordinates
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        
+        // Save city name and coordinates to local storage
+        var coordinates = lat + ` ` + lon;
+        localStorage.setItem(city, coordinates);
+
+        // Fetch new data due to coordinates we have
+        var queryUrl = `https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`
+        fetch(queryUrl)
+        .then(function (newResponse) {
+          return newResponse.json();
+        })
+        .then(function (newData) {
+          console.log(newData);
+          currentWeather(newData);
+          futureWeather(newData);
+
+        });
+      })
+}
+
+fetchWeatherInfo() 
+
+// Current Weather Information 
+var currentTime = dayjs().format(`HH`);
+var timeOfTheDay = ``;
+console.log(currentTime);
+if (currentime > 17) {
+  timeOfTheDay === 5;
+} else {
+  timeOfTheDay === 3;
+}
+// 0 = 06:00 
+// 1 = 09:00 
+// 2 = 12:00 
+// 3 = 15:00 
+// 4 = 18:00 
+// 5 = 21:00 
+// 6 = 00:00 
+// 7 = 03:00 
+function currentWeather(newData) {
+  var temp = newData.list[timeOfTheDay].main.temp.toFixed(`2`);
+  var wind = newData.list[timeOfTheDay].wind.speed.toFixed(`2`);
+  var humidity = newData.list[timeOfTheDay].main.humidity.toFixed();
+  var date = dayjs.unix(newData.list[timeOfTheDay].dt).format(`D[/]MM[/]YYYY`);  
+  var iconUrl = `http://openweathermap.org/img/wn/${newData.list[timeOfTheDay].weather[0].icon}@2x.png`;
+  $(`#city-name`).text(`${city} (${date})`);
+  $(`#temperature`).text(`Temperature : ${temp} °C`);
+  $(`#wind`).text(`Wind Speed : ${wind} kmh`);
+  $(`#humidity`).text(`Humidity : ${humidity}%`);
+  $(`#icon`).attr(`src`, iconUrl);
+}
+
+
+$(`#search-button`).on(`click`, function () {
+  fetchWeatherInfo();
+})
+
+// Future Weather Information 
+function futureWeather(newData) {
+  for (var i = timeOfTheDay; i<= 30 ; i++) {
+    var temp5 = newData.list[i].main.temp.toFixed(`2`);
+    var wind5 = newData.list[i].wind.speed.toFixed(`2`);
+    var humidity5 = newData.list[i].main.humidity.toFixed();
+    var date5 = newData.list[i].dt_txt;
+    
+    var iconUrl5 = `http://openweathermap.org/img/wn/${newData.list[i].weather[0].icon}@2x.png`;
+    var futureWeatherDiv = $(`<div>`);
+    var futureWeatherUl = $(`<ul>`);
+    var cityNameLi = $(`<li class='city-name 5dayforecast'>`);
+    var temperatureLi = $(`<li class='tempetature 5dayforecast'>`);
+    var windLi = $(`<li class='wind 5dayforecast'>`);
+    var humidityLi = $(`<li class='humidity 5dayforecast'>`);
+    var dateFormatted = dayjs(newData.list[i].dt_txt, "YYYY-MM-DD HH:mm:ss").format(`D MM YYYY`);
+    cityNameLi.text(dateFormatted);
+    temperatureLi.text(`Temperature : ${temp5} °C`);
+    windLi.text(`Wind Speed : ${wind5} kmh`);
+    humidityLi.text(`Humidity : ${humidity5}%`);
+    var d5ayicon = $(`<img>`)
+    d5ayicon.attr(`src`, iconUrl5);
+    $(`#forecast`).append(futureWeatherDiv);
+    futureWeatherDiv.append(futureWeatherUl);
+    futureWeatherUl.append(cityNameLi,temperatureLi,windLi,humidityLi);
+  } // dayjs.unix(newData.list[i].dt).format(`D[/]MM[/]YYYY`);  
+}
+$(`#search-button`).on(`click`, function () {
+  fetchWeatherInfo();
+})
