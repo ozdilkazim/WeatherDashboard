c506ec288d313bbbd5cd927b40306c06181800c7
Kazim Ozdil <skozdil@gmail.com>
2024-01-30 04:29:38 +0000

    Local storage save and fetch fucntions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,78 @@
 // Create cities array
-var city = `London`;
 var cities = [];
 var apiKey = `71311474f5b26fb7bbfa0bc1985b90cd`;
 
-// Fetch weather info for London (default city)
+// Get past searches if applicaple
+getPastSearches();
+
+// Fetch informatin for default city
+city = `London`;
 fetchWeatherInfo();
-//var city = `London`;
+
 $("#search-button").on("click", function (event) {
   $("#forecast").empty();
+  $("#history").empty();
   event.preventDefault();
   // This line grabs the input from the textbox
   city = toTitleCase($("#search-input").val().trim());
+  addPastSeachList(city);
   fetchWeatherInfo();
+  getPastSearches();
 });
 
+function addPastSeachList(arr) {
+  if (arr =``) {
+    alert=`City name cannot be blank!`;
+  } else {
+    var addToList = $("<button type='button' class='btn btn-light past-search' data-name='"+ arr +"'>");
+    addToList.text(arr);
+    $("#history").append(addToList);
+  }
+}
 
 function fetchWeatherInfo() {
-    // Convert city name to coordtinats using geo api
-    var queryUrlGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
-    fetch(queryUrlGeo)
-      .then(function (response) {
-
-        if (response.ok) {
-          response.json().          
-          then(function (data) {
-            // Coordinates
-            var lat = data[0].lat;
-            var lon = data[0].lon;
-            
-            // Save city name and coordinates to local storage
-            var coordinates = lat + ` ` + lon;
-            localStorage.setItem(city, coordinates);
+  // Convert city name to coordtinats using geo api
+  var queryUrlGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
+  fetch(queryUrlGeo).then(function (response) {
+    console.log(response);
+      return response.json();
+  }).then(function (data) {
+    // Coordinates
+    var lat = data[0].lat;
+    var lon = data[0].lon;
     
-            // Fetch new data due to coordinates we have
-            var queryUrl = `https://api.openweathermap.org/data/2.5/onecall?units=metric&exclude=minutely,hourly&lat=${lat}&lon=${lon}&appid=${apiKey}`
-            fetch(queryUrl)
-            .then(function (newResponse) {
-              if (response.ok) {
-                newResponse.json()
-                .then(function (newData) {
-                currentWeather(newData);
-                console.log(newData);
-                });
-              }
-            });
-          });
-
-        } else {
-
-          alert(`Error, please enter correct city name!`);
-
-        }
-      });
+    // Save city name and coordinates to local storage
+    var storage = {
+      cityName : city,
+      latitude : lat,
+      longitude : lon,
     }
+    localStorage.setItem(city, JSON.stringify(storage));
+
+    // Fetch new data due to coordinates we have
+    var queryUrl = `https://api.openweathermap.org/data/2.5/onecall?units=metric&exclude=minutely,hourly&lat=${lat}&lon=${lon}&appid=${apiKey}`
+    fetch(queryUrl).then(function (newResponse) {
+      console.log(newResponse);
+      if (newResponse.ok) {
+        newResponse.json()
+        .then(function (newData) {
+        currentWeather(newData);
+        console.log(newData);
+        });
+      } else {
+        alert(`Please enter a valid city name!`);
+      }
+    });
+  });  
+};
 
 function currentWeather(data) {
   var temp = data.current.temp.toFixed(`2`);
   var wind = data.current.wind_speed.toFixed(`2`);
   var humidity = data.current.humidity.toFixed();
-  var date = dayjs.unix(data.current.dt).format(`D[/]MM[/]YYYY`);  
   var iconUrl = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
-  $(`#city-name`).text(`${city} (${date})`);
+  var date = dayjs.unix(data.current.dt).format(`D[/]MM[/]YYYY`);  
+  $("#city-name").text(`${city} (${date})`);
   $(`#temperature`).text(`Temperature : ${temp} °C`);
   $(`#wind`).text(`Wind : ${wind} kph`);
   $(`#humidity`).text(`Humidity : ${humidity}%`);
@@ -102,3 +114,40 @@ function toTitleCase(str) {
     }
   );
 }
+
+// Get past searches from local storage
+function getPastSearches() {
+  // Get city keys first
+  var cityKeys = Object.keys(localStorage);
+  console.log(cityKeys)
+  //Get coordinates by city names
+  for (var i=0; i < localStorage.length; i++) {
+    cities[i] = JSON.parse(localStorage.getItem(cityKeys[i]));
+    var pastSearchButton = $("<button type='button' class='btn btn-light past-search' data-name='"+ cityKeys[i] +"'>");
+    pastSearchButton.text(cityKeys[i]);
+    $("#history").append(pastSearchButton);
+  }
+}
+
+// Get past search information from local storage
+function getPastSearchInfo(event) {
+  event.preventDefault();
+  $("#forecast").empty();
+  $("#history").empty();
+  city = $(this).attr("data-name");
+  var pastLat = JSON.parse(localStorage.getItem(city)).latitude;
+  var pastLon = JSON.parse(localStorage.getItem(city)).longitude;
+  // Fetch new data due to coordinates we have
+  var queryUrl = `https://api.openweathermap.org/data/2.5/onecall?units=metric&exclude=minutely,hourly&lat=${pastLat}&lon=${pastLon}&appid=${apiKey}`
+    fetch(queryUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    currentWeather(data);
+  });
+  getPastSearches();
+}
+
+// Get past searches information when clicked
+$(document).on("click", ".past-search", getPastSearchInfo);
